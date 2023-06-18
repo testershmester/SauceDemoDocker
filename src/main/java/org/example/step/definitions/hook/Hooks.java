8c1ca6821c9ef7875cad4ddb919e2675dd5ea630
Katsiaryna Zharskaya <katsiaryna_zharskaya@epam.com>
2023-06-18 19:52:36 +0300

    fix

--- src/main/java/org/example/step/definitions/hook/Hooks.java
+++ src/main/java/org/example/step/definitions/hook/Hooks.java
@@ -4,6 +4,9 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.page.LoginPage;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashMap;
@@ -15,8 +18,11 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class Hooks {
 
+    private final Logger log = LogManager.getLogger(LoginPage.class);
+
     @Before
     public void setUpBrowser(Scenario scenario) {
+        log.info("Before hook");
         browser = CHROME;
         browserVersion = "113.0";
         browserSize = "1920x1080";
@@ -24,7 +30,7 @@ public class Hooks {
         options.setCapability("browserVersion", "113.0");
         options.setCapability("selenoid:options", new HashMap<String, Object>() {{
             put("enableVideo", true);
-            put("videoName", StringUtils.toRootLowerCase(scenario.getName()).replace(" ", "_"));
+//            put("videoName", StringUtils.toRootLowerCase(scenario.getName()).replace(" ", "_"));
         }});
         browserCapabilities = options;
     }
